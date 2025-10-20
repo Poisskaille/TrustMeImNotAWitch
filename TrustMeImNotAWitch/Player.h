5dc2a1e8996dae610c499cf98a717b901dcd9807
Poasskaille <amoreau@GCFRPA055>
2025-10-20 12:46:55 +0200

    player movement QS / Jump

--- TrustMeImNotAWitch/Player.h
+++ TrustMeImNotAWitch/Player.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 #include "SFML/Graphics.hpp"
 
 enum class State{GROUNDED, JUMPING};
@@ -13,13 +14,23 @@ public:
 	void Update(float dT);
 	void Draw(sf::RenderWindow& window);
 	void HandleInput();
+	void Jump();
+
+	void Collision();
 
 private:
 
 	State playerState;
 	sf::RectangleShape playerShape;
+
+	sf::Vector2f velocity;
 	float speed;
 	float deltaTime;
+	float jumpForce;
+	float gravity;
+
+	// A supprimer plus tard, juste pour test
+	sf::RectangleShape ground;
 	
 };
 
