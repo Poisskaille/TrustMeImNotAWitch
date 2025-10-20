4687cd89e09d56b1dc86813e87ac6e2ee8bfaf6e
Poasskaille <amoreau@GCFRPA055>
2025-10-20 11:44:38 +0200

    class Game

--- TrustMeImNotAWitch/TrustMeImNotAWitch.cpp
+++ TrustMeImNotAWitch/TrustMeImNotAWitch.cpp
@@ -1,29 +1,9 @@
 #include <iostream>
-#include "SFML/Graphics.hpp"
-
-#include "Player.h"
+#include "Game.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 1920, 1080 }), "Trust me im not a Witch");
-    Player player;
-
-    sf::Clock clock;
-
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-        }
-
-        float deltaTime = clock.getElapsedTime().asSeconds();
-        clock.restart();
-
-        window.clear();
-        player.Update(deltaTime);
-        player.Draw(window);
-        window.display();
-    }
+	Game* game = new Game;
+	game->run();
+	delete game;
 }
