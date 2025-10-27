722483b1b572f9863145363236f6718b28ceac3a
Poasskaille <amoreau@GCFRPA055>
2025-10-27 12:10:17 +0100

    Fix camera offset

--- TrustMeImNotAWitch/CameraUI.cpp
+++ TrustMeImNotAWitch/CameraUI.cpp
@@ -1,6 +1,6 @@
 #include "CameraUI.h"
 
-CameraUI::CameraUI(sf::Vector2f pos): Y_OFFSET(pos.y + 400)
+CameraUI::CameraUI(sf::Vector2f pos) : Y_OFFSET(pos.y + 75)
 {
 	cam.setSize({ 800.f,400.f });
 	cam.zoom(1.5f);
@@ -36,11 +36,10 @@ void CameraUI::drawUI(sf::RenderWindow& window)
 
 void CameraUI::initUI()
 {
-	UIManagers::getInstance().createUI(UI_TYPE::IMAGE,UI_LIST::CAMERA,sf::Vector2f(cam.getCenter().x, (cam.getSize().y / 2) - 50), sf::Vector2f(256, 128), sf::Color::Blue, "Test",'s');
+	UIManagers::getInstance().createUI(UI_TYPE::IMAGE, UI_LIST::CAMERA, sf::Vector2f(cam.getCenter().x, (cam.getSize().y / 2) - 50), sf::Vector2f(256, 128), sf::Color::Blue, "Test", 's');
 }
 
 sf::View CameraUI::getCam()
 {
 	return cam;
 }
-
